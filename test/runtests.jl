bef11744e9654965f8347bd66fe9bcd5ed775d77
Nick Robinson <npr251@gmail.com>
2023-01-24 15:56:29 +0000

    Test testitems inherit group

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,11 @@ end
 @testsetup TS1 begin
     x = 1
 end
-
 @test ReTestItems.gettestsetup(:TS1).name == :TS1
 
 @testitem "TI1" begin
     @test 1 + 1 == 2
 end
-
 @test ReTestItems.gettestitem("TI1").name == "TI1"
 @test ReTestItems.gettestitem("TI1").file == @__FILE__
 
@@ -21,6 +19,17 @@ end
     @test true
 end
 @test ReTestItems.gettestitem("TI2").tags == [:foo]
+
+@testitemgroup "GroupTests" begin
+    @testitem "TI_grouped" begin
+        @test 1 + 1 == 2
+    end
+end
+@testitem "TI_ungrouped" begin
+    @test 1 + 1 == 2
+end
+@test ReTestItems.gettestitem("TI_grouped").group == "GroupTests"
+@test ReTestItems.gettestitem("TI_ungrouped").group == nothing
 
 @testsetup FooSetup begin
     x = 1
