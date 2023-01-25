89503430fcbe92078bed4c92e1c710a642f65131
Jacob Quinn <quinn.jacobd@gmail.com>
2023-01-24 23:43:15 -0700

    Lots more work towards fully working test item running (#6)
    * WIP
    
    * Pick up where Nick left off and push things forward
    
    * Get test setup eval + plumbing implemented
    * Add filtering support to runtests
    * stub out distributed parallel evaluation of test items
    
    Co-authored-by: Nick Robinson <npr251@gmail.com>

--- src/ReTestItems.jl
+++ src/ReTestItems.jl
@@ -1,7 +1,11 @@
 module ReTestItems
 
+using Test: Test, DefaultTestSet
+using Distributed: RemoteChannel, @spawnat, nprocs, workers
+using .Threads: @spawn
+
 export @testitemgroup, @testsetup, @testitem,
-       TestSetup, TestItem
+       TestSetup, TestItem, runtestitem
 
 # pass in a type T, value, and str key
 # we get a Dict{String, T} from task_local_storage
@@ -22,14 +26,28 @@ test items are run together.
 """
 macro testitemgroup(name, body)
     esc(quote
-        task_local_storage(:__TEST_ITEM_GROUP__, $name) do
+        task_local_storage(:__RE_TEST_ITEM_GROUP__, $name) do
             $body
         end
     end)
 end
 
 # get any test item group that has been set in task_local_storage
-gettestitemgroup() = get(task_local_storage(), :__TEST_ITEM_GROUP__, nothing)
+gettestitemgroup() = get(task_local_storage(), :__RE_TEST_ITEM_GROUP__, nothing)
+
+"""
+    SetupSet()
+
+A set of test setups. Used to keep track of
+which test setups have been evaluated on a given
+process.
+"""
+struct SetupSet
+    lock::ReentrantLock
+    modules::Dict{Symbol, Module} # set of @testsetup modules that have already been evaled
+end
+
+SetupSet() = SetupSet(ReentrantLock(), Dict{Symbol, Module}())
 
 struct TestSetup
     name::Symbol
@@ -53,6 +71,35 @@ end
 # retrieve a test setup by name
 gettestsetup(name) = task_local_storage(nameof(TestSetup))[String(name)]
 
+"""
+    SetupContext()
+
+A context for test setups. Used to keep track of
+`@testsetup` expanded `TestSetup` and a `SetupSet`
+for a given process; used in `runtestitem` to ensure
+any setups relied upon by the `@testitem` are evaluated
+on the process that will run the test item.
+"""
+mutable struct SetupContext
+    setups::Dict{String, TestSetup}
+    setupset::SetupSet
+
+    # user of SetupContext must create and set the
+    # SetupSet explicitly, since they must be process-local
+    # and shouldn't be serialized across processes
+    SetupContext() = new(Dict{String, TestSetup}())
+end
+
+# NOTE: TestItems are serialized across processes for
+# distributed testing, so care needs to be taken that
+# fields are serialization-appropriate and process-local
+# state is managed externally like SetupContext in runtests
+"""
+    TestItem
+
+A single, independently runnable group of tests.
+Analogous to a `@testset`. 
+"""
 struct TestItem
     name::String
     tags::Vector{Symbol}
@@ -92,11 +139,182 @@ macro testitem(nm, exs...)
         error("expected @testitem to have a body")
     end
     q = QuoteNode(exs[end])
+    ti = gensym()
     esc(quote
-        $settls!($TestItem, $TestItem($nm, $tags, $default_imports, $setup, $(String(__source__.file)), $q, ReTestItems.gettestitemgroup()), $nm)
+        $ti = $TestItem($nm, $tags, $default_imports, $setup, $(String(__source__.file)), $q, $(gettestitemgroup)())
+        $settls!($TestItem, $ti, $nm)
+        if haskey(task_local_storage(), :__RE_TEST_CHANNEL__)
+            put!(task_local_storage(:__RE_TEST_CHANNEL__), $ti)
+        end
+        $ti
     end)
 end
 
 gettestitem(name) = task_local_storage(nameof(TestItem))[String(name)]
+
+# get the /src and /test directories for a project/package
+function _src_and_test(root_dir)
+    src_dir = joinpath(root_dir, "src")
+    test_dir = joinpath(root_dir, "test")
+    # we don't check isdir here since it happens later in include_testfiles!
+    return (src_dir, test_dir)
+end
+
+"""
+    ReTestItems.runtests()
+    ReTestItems.runtests(mod::Module)
+    ReTestItems.runtests(dirs...)
+
+Execute `@testitem` tests. If no arguments are passed, the current
+project is searched for `src` and `test` directories. If a module
+is passed, the `src` and `test` directories are searched for that
+module. If directories are passed, they are searched directly for
+`@testitem`s.
+"""
+function runtests end
+
+default_shouldrun(ti::TestItem) = true
+
+runtests(dirs::String...) = runtests(default_shouldrun, dirs...)
+
+runtests(pkg::Module) = runtests(default_shouldrun, pkg)
+function runtests(shouldrun, pkg::Module)
+    dir = pkgdir(pkg)
+    isnothing(dir) && error("Could not find directory for $pkg")
+    return runtests(shouldrun, _src_and_test(dir)...)
+end
+
+chan(ch::RemoteChannel) = channel_from_id(remoteref_id(ch))
+
+# FilteredChannel applies a filtering function `f` to items
+# when you try to `put!` and only puts if `f` returns true.
+struct FilteredChannel{F, T}
+    f::F
+    ch::T
+end
+
+Base.put!(ch::FilteredChannel, x) = ch.f(x) && put!(ch.ch, x)
+
+function runtests(shouldrun, dirs::String...)
+    if isempty(dirs)
+        dirs = (dirname(Base.active_project()),)
+    end
+    setupctx = SetupContext()
+    ch = RemoteChannel(() -> Channel{TestItem}(Inf))
+    # run test file including @spawn so we can immediately
+    # start executing tests as they're put into our test channel
+    fch = FilteredChannel(shouldrun, chan(ch))
+    Threads.@spawn include_testfiles!($(setupctx.setups), $fch, dirs...)
+    if nprocs() == 1
+        schedule_testitems!(setupctx, chan(ch))
+    else
+        futures = [@spawnat(p, schedule_testitems!(setupctx, ch)) for p in workers()]
+        foreach(wait, futures)
+    end
+end
+
+# for each directory, kick off a recursive test-finding task
+function include_testfiles!(setups::Dict{String, TestSetup}, ch::FilteredChannel, dirs...)
+    @sync for dir in dirs
+        isdir(dir) || error("`$dir` directory doesn't exist")
+        for (root, dir, files) in walkdir(dir)
+            for file in files
+                # channel should only be closed if there was an error
+                isopen(ch) || return
+                @show file
+                endswith(file, "_test.jl") || continue
+                filepath = joinpath(root, file)
+                Threads.@spawn begin
+                    try
+                        task_local_storage(:__RE_TEST_CHANNEL__, $ch) do
+                            # ensure any `@testsetup` macros are expanded into
+                            # our setups Dict
+                            task_local_storage(nameof(TestSetup), $setups) do
+                                include($filepath)
+                            end
+                        end
+                    catch e
+                        @error "error including test file $filepath" exception=(e, catch_backtrace())
+                        # use the exception to close our TestItem channel so it gets propagated
+                        close($ch, e)
+                    end
+                end
+            end
+        end
+    end
+    # because we @sync waited on all our spawned tasks to finish, we know
+    # at this point, we've finished including all test files in dirs, so
+    # now we close our test channel to signal that no more will be added
+    close(ch)
+    return
+end
+
+function schedule_testitems!(setupctx::SetupContext, ch::Channel)
+    setupctx.setupset = SetupSet()
+    @sync for ti in ch
+        Threads.@spawn runtestitem($ti, $setupctx)
+    end
+    return
+end
+
+function schedule_testitems!(setupctx::SetupContext, ch::RemoteChannel)
+    setupctx.setupset = SetupSet()
+    while true
+        try
+            ti = take!(ch)
+            runtestitem(ti, setupctx)
+            !isopen(ch) && !isready(ch) && break
+        catch e
+            if isa(e, InvalidStateException) && !isopen(ch)
+                break
+            else
+                rethrow(e)
+            end
+        end
+    end
+    return
+end
+
+function ensure_setup!(setupctx::SetupContext, setup::Symbol)
+    Base.@lock setupctx.setupset.lock begin
+        if !haskey(setupctx.setupset.setups, setup)
+            # we haven't eval-ed this setup module yet
+            ts = setupctx.setups[String(setup)]
+            mod = Core.eval(Main, :(module $(gensym(ts.name)) end))
+            setupctx.setupset.setups[setup] = mod
+        end
+        return nameof(setupctx.setupset.setups[setup])
+    end
+end
+
+# convenience method for testing
+# assumes any required setups were expanded in current task_local_storage
+function runtestitem(ti::TestItem)
+    ctx = SetupContext()
+    ctx.setups = get(() -> Dict{String, TestSetup}(), task_local_storage(), nameof(TestSetup))
+    ctx.setupset = SetupSet()
+    runtestitem(ti, ctx)
+end
+
+function runtestitem(ti::TestItem, setupctx::SetupContext)
+    mod = Core.eval(Main, :(module $(gensym(ti.name)) end))
+    if ti.default_imports
+        Core.eval(mod, :(using Test))
+        # TODO: know which package a TestItem belongs to.
+        # if ti.package !== nothing
+        #     Core.eval(mod, :(using $(ti.package)))
+        # end
+    end
+    for setup in ti.setup
+        # ensure setup has been evaled before
+        ts_mod = ensure_setup!(setupctx, setup)
+        # eval using in our @testitem module
+        Core.eval(mod, :(using $ts_mod))
+    end
+    Test.push_testset(DefaultTestSet(ti.name; verbose=true))
+    Core.eval(mod, ti.code)
+    Test.finish(Test.pop_testset())
+    return
+end
 
 end # module ReTestItems
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using ReTestItems, Test
 
+# this file specifically tests *unit* tests for ReTestItems
+# so *not* the `runtests` functionality, which utilizes specific
+# contrived packages/testfiles
+
 @testitemgroup "Group1" begin
     @test ReTestItems.gettestitemgroup() == "Group1"
 end
@@ -9,16 +13,18 @@ end
 end
 @test ReTestItems.gettestsetup(:TS1).name == :TS1
 
-@testitem "TI1" begin
+ti = @testitem "TI1" begin
     @test 1 + 1 == 2
 end
 @test ReTestItems.gettestitem("TI1").name == "TI1"
 @test ReTestItems.gettestitem("TI1").file == @__FILE__
+@test ReTestItems.runtestitem(ti) === nothing
 
-@testitem "TI2" tags=[:foo] begin
+ti2 = @testitem "TI2" tags=[:foo] begin
     @test true
 end
 @test ReTestItems.gettestitem("TI2").tags == [:foo]
+@test ReTestItems.runtestitem(ti2) === nothing
 
 @testitemgroup "GroupTests" begin
     @testitem "TI_grouped" begin
@@ -29,12 +35,14 @@ end
     @test 1 + 1 == 2
 end
 @test ReTestItems.gettestitem("TI_grouped").group == "GroupTests"
-@test ReTestItems.gettestitem("TI_ungrouped").group == nothing
+@test ReTestItems.gettestitem("TI_ungrouped").group === nothing
+@test ReTestItems.runtestitem(ReTestItems.gettestitem("TI_grouped")) === nothing
+@test ReTestItems.runtestitem(ReTestItems.gettestitem("TI_ungrouped")) === nothing
 
 @testsetup FooSetup begin
     x = 1
 end
 @testitem "Foo" setup=[FooSetup] begin
-    @test true
+    @test FooSetup.x == 1
 end
 @test ReTestItems.gettestitem("Foo").setup == [:FooSetup]
