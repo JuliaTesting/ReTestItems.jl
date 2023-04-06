95b99adda5261a6c06824b2372696e21d0eda6eb
Nick Robinson <npr251@gmail.com>
2023-04-06 01:15:32 +0100

    Always start new worker after timeout or crash (#33)
    * Always start new worker after timeout or crash
    
    * fixup! Always start new worker after timeout or crash
    
    * fixup! fixup! Always start new worker after timeout or crash
    
    * Bump version
    
    * fixup! fixup! fixup! Always start new worker after timeout or crash

--- test/_abort_tests.jl
+++ test/_abort_tests.jl
@@ -4,3 +4,11 @@ using ReTestItems
     # we're explicitly crashing the worker here
     ccall(:abort, Cvoid, ())
 end
+
+# We want to test that workers which crash get replaced.
+# Here we add a test item that should run and pass even after the testitem above crashes.
+# For this test to be valid, there must be exactly one worker and this testitem must be
+# scheduled _after_ the previous testitem ran and crashed the worker.
+@testitem "Test which should run after abort" begin
+    @test true
+end
