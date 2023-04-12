72630f5259be53f26698339a8a2b0b8043de05e3
Michael Arntzenius <daekharel@gmail.com>
2023-04-12 18:04:17 +0200

    two birds, one stone

--- src/testcontext.jl
+++ src/testcontext.jl
@@ -151,8 +151,10 @@ Test.finish(ti::TestItems) = Test.finish(ti.graph.testset)
 
 function get_starting_testitems(ti::TestItems, n)
     # we want to select n evenly spaced test items from ti.testitems
-    step = round(Int, length(ti.testitems) / n)
-    testitems = ti.testitems[1:step:end]
+    len = length(ti.testitems)
+    step = max(1, len / n)
+    testitems = [ti.testitems[round(Int, i)] for i in 1:step:len]
+    @assert length(testitems) == min(n, len) && allunique(testitems)
     for (i, t) in enumerate(testitems)
         @atomic t.scheduled_for_evaluation.value = true
         # mark eval_number
