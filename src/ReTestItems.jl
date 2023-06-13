8f89ab028b7e59cad31f0d696fed14fdff5cc5ea
Nick Robinson <npr251@gmail.com>
2023-06-13 13:00:58 +0100

    fixup! Call GC directly

--- src/ReTestItems.jl
+++ src/ReTestItems.jl
@@ -419,7 +419,7 @@ function start_and_manage_worker(
                 print_errors_and_captured_logs(testitem, nretries + 1; logs)
                 report_empty_testsets(testitem, ts)
                 # Run GC to free memory on the worker before next testitem.
-                remote_fetch(worker, :(GC.gc(true); GC.gc(false))
+                remote_fetch(worker, :(GC.gc(true); GC.gc(false)))
                 # If the result isn't a pass, we throw to go to the outer try-catch
                 throw_if_failed(ts)
                 testitem = next_testitem(testitems, testitem.id[])
