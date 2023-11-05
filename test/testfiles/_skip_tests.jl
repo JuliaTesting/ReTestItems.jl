414b7b7eaf84e6a79e935cc5ce6f98597325d85f
Nick Robinson <npr251@gmail.com>
2023-11-05 13:59:22 +0000

    WIP integration tests for skipping testitems

--- test/testfiles/_skip_tests.jl
+++ test/testfiles/_skip_tests.jl
@@ -1,21 +1,21 @@
+# one test, which is run.
 @testitem "1" begin
     @test true
 end
+# two tests, but whole testitem skipped.
 @testitem "2" skip=true begin
     @test true
-end
-@testitem "3" skip=VERSION < v"2" begin
     @test true
 end
-@testitem "4" skip=VERSION > v"2" begin
+# skip expression, returns true
+@testitem "3" skip=VERSION < v"3" begin
+    no_existent_func()
+end
+# skip expression, returns false
+@testitem "4" skip=VERSION > v"3" begin
     @test true
 end
-@testitem "5" skip=:(using Example; Example.domath(1) isa Number) begin
+# multi-line skip expression, returns true
+@testitem "5" skip=:(using AutoHasEquals; AutoHasEquals isa Module) begin
     @test true
 end
-# @testitem "5b" skip=:(using Base; Example.domath(1) isa Number) begin
-#     @test true
-# end
-# @testitem "6" skip=:(1+1) begin
-#     @test true
-# end
