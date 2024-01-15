3b52add92c3fbb9ea59fcb2cc9e97d22dd9c3262
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2024-01-15 13:19:00 +0100

    More fixes and tweaks

--- ext/JETExt.jl
+++ ext/JETExt.jl
@@ -2,12 +2,12 @@ module JETExt
 
 import JET, ReTestItems, Test
 
-function _analyze_toplevel(ex, file, mode)
+function _analyze_toplevel(ex, file, mode, ignore_modules)
     toplevelex = Expr(:toplevel, ex)
-    analyzer = JET.JETAnalyzer(; mode)
-    config = JET.ToplevelConfig(; mode)
+    analyzer = JET.JETAnalyzer(; mode, ignore_missing_comparison=true)
+    config = JET.ToplevelConfig(; concretization_patterns = [])
     res = JET.virtual_process(toplevelex, file, analyzer, config)
-    return JET.JETToplevelResult(analyzer, res, "analyze_toplevel"; mode)
+    return JET.JETToplevelResult(analyzer, res, "analyze_toplevel"; mode, ignore_modules, target_defined_modules=false)
 end
 
 function ReTestItems.jet_test(ti::ReTestItems.TestItem, mod_expr::Expr, jet::Symbol)
@@ -17,14 +17,13 @@ function ReTestItems.jet_test(ti::ReTestItems.TestItem, mod_expr::Expr, jet::Sym
 
     @assert mod_expr.head === :module "Expected the test item expression to be wrapped in a module, got $(repr(mod_expr.head))"
     Test.@testset "JET $(repr(jet)) mode" begin
-        result = _analyze_toplevel(mod_expr, ti.file, jet)
+        result = _analyze_toplevel(mod_expr, ti.file, jet, (JET.AnyFrameModule(Test), JET.AnyFrameModule(JET)))
         reports = JET.get_reports(result)
-        filtered_reports = JET.configured_reports(reports, ignored_modules=(JET.AnyFrameModule(Test), JET.AnyFrameModule(JET)))
-        no_jet_errors = isempty(filtered_reports)
+        no_jet_errors = isempty(reports)
         onfail(Test.@test no_jet_errors) do
             JET.print_reports(
                 stdout,
-                filtered_reports,
+                reports,
                 # Remove the name of the module JET uses for virtualization the code and the name of the module
                 # we wrap the test items in.
                 JET.gen_postprocess(result.res.actual2virtual) ∘ x->replace(x, string("var\"", mod_expr.args[2], "\"") => ""),
