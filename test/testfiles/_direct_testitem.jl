0d4a6c5c2586f813bba5c3faa9124499f8aba2dd
Nick Robinson <npr251@gmail.com>
2024-09-18 10:45:42 +0100

    Update test/testfiles/_direct_testitem.jl

--- test/testfiles/_direct_testitem.jl
+++ test/testfiles/_direct_testitem.jl
@@ -1,6 +1,6 @@
 # This file is incorrect usage of ReTestItems
 # but we want to check that a failed `@testitem` used directly in a script
-# means the script exists with a non-zero exit code.
+# means the script exits with a non-zero exit code.
 using ReTestItems
 @testitem "should fail" begin
     @test 1 == 2
